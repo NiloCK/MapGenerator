066a970b1c690f37edced76a7f72d2029885dc68
cmkcmk <kolinkennedy@gmail.com>
2015-02-09 06:09:14 -0330

    Gone .NET since the API that's serving my data doesn't seem to enjoy serving client side JS. (This time actually committing things)

---
+++ Maps/SuggestionSniffer/Sniffer.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuggestionSniffer
+{
+    public class Sniffer
+    {
+        public static List<String> Extensions(String query)
+        {
+            if (query.Contains("{state}"))
+            {
+                throw new ArgumentException("Query must contain '{state}'.");
+            }
+            
+            WebRequest req = WebRequest.CreateHttp("http://suggestqueries.google.com/complete/search");
+            req.Method = "GET";
+            WebClient client;
+            WebClient client = new WebClient();
+            client.QueryString = new System.Collections.Specialized.NameValueCollection(2);
+
+
+
+            return null;
+        }
+
+        private static readonly IDictionary<string, string> states = new Dictionary<string, string>
+        {
+            { "AL", "Alabama" },
+            { "AK", "Alaska" },
+            { "AZ", "Arizona" },
+            { "AR", "Arkansas" },
+            { "CA", "California" },
+            { "CO", "Colorado" },
+            { "CT", "Connecticut" },
+            { "DE", "Delaware" },
+            { "DC", "District of Columbia" },
+            { "FL", "Florida" },
+            { "GA", "Georgia" },
+            { "HI", "Hawaii" },
+            { "ID", "Idaho" },
+            { "IL", "Illinois" },
+            { "IN", "Indiana" },
+            { "IA", "Iowa" },
+            { "KS", "Kansas" },
+            { "KY", "Kentucky" },
+            { "LA", "Louisiana" },
+            { "ME", "Maine" },
+            { "MD", "Maryland" },
+            { "MA", "Massachusetts" },
+            { "MI", "Michigan" },
+            { "MN", "Minnesota" },
+            { "MS", "Mississippi" },
+            { "MO", "Missouri" },
+            { "MT", "Montana" },
+            { "NE", "Nebraska" },
+            { "NV", "Nevada" },
+            { "NH", "New Hampshire" },
+            { "NJ", "New Jersey" },
+            { "NM", "New Mexico" },
+            { "NY", "New York" },
+            { "NC", "North Carolina" },
+            { "ND", "North Dakota" },
+            { "OH", "Ohio" },
+            { "OK", "Oklahoma" },
+            { "OR", "Oregon" },
+            { "PA", "Pennsylvania" },
+            { "RI", "Rhode Island" },
+            { "SC", "South Carolina" },
+            { "SD", "South Dakota" },
+            { "TN", "Tennessee" },
+            { "TX", "Texas" },
+            { "UT", "Utah" },
+            { "VT", "Vermont" },
+            { "VA", "Virginia" },
+            { "WA", "Washington" },
+            { "WV", "West Virginia" },
+            { "WI", "Wisconsin" },
+            { "WY", "Wyoming" }
+        };
+    }
+}
